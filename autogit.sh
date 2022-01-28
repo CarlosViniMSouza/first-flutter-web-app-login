5f4d557c456f2b72bd356349b6c8009a7964360a
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-01-28 17:57:16 -0300

    Step 3: Add animation for sign in progress

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,6 @@
 git init
 git add .
-git commit -m "Step 2 - Enable sign in progress tracking"
+git commit -m "Step 3: Add animation for sign in progress"
 git branch -M master
 git remote add origin https://github.com/CarlosViniMSouza/first-flutter-web-app-login.git
 git push -u origin master
