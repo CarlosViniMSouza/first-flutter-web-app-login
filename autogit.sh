3ec377cd9a793e9110c85c2cb35b735bb660d8d5
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-01-28 15:32:28 -0300

    Project inserted with successfully

---
+++ autogit.sh
@@ -0,0 +1,7 @@
+git init
+git add .
+git commit -m "Project inserted with successfully"
+git branch -M master
+git remote add origin https://github.com/CarlosViniMSouza/first-flutter-web-app-login.git
+git push -u origin master
+git status
---
+++ lib/main.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const SignUpApp());
+
+class SignUpApp extends StatelessWidget {
+  // ignore: use_key_in_widget_constructors
+  const SignUpApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        '/': (context) => const SignUpScreen(),
+        '/welcome': (context) => const WelcomeScreen(),
+      },
+    );
+  }
+}
+
+class SignUpScreen extends StatelessWidget {
+  // ignore: use_key_in_widget_constructors
+  const SignUpScreen();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: const Center(
+        child: SizedBox(
+          width: 400,
+          child: Card(
+            child: SignUpForm(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class WelcomeScreen extends StatelessWidget {
+  // ignore: use_key_in_widget_constructors
+  const WelcomeScreen();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Bem-Vindo ao meu app!', style: Theme.of(context).textTheme.headline2),
+      ),
+    );
+  }
+}
+
+
+class SignUpForm extends StatefulWidget {
+  // ignore: use_key_in_widget_constructors
+  const SignUpForm();
+
+  @override
+  _SignUpFormState createState() => _SignUpFormState();
+}
+
+class _SignUpFormState extends State<SignUpForm> {
+  final _firstNameTextController = TextEditingController();
+  final _lastNameTextController = TextEditingController();
+  final _usernameTextController = TextEditingController();
+
+  final double _formProgress = 0;
+
+  void _showWelcomeScreen() {
+    Navigator.of(context).pushNamed('/welcome');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          LinearProgressIndicator(value: _formProgress),
+          Text('Sign up', style: Theme
+              .of(context)
+              .textTheme
+              .headline4),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              controller: _firstNameTextController,
+              decoration: const InputDecoration(hintText: 'Nome'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              controller: _lastNameTextController,
+              decoration: const InputDecoration(hintText: 'Sobrenome'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              controller: _usernameTextController,
+              decoration: const InputDecoration(hintText: 'Nome Usuario'),
+            ),
+          ),
+          TextButton(
+            style: ButtonStyle(
+              foregroundColor: MaterialStateProperty.resolveWith((Set<MaterialState> states) {
+                return states.contains(MaterialState.disabled) ? null : Colors.white;
+              }),
+              backgroundColor: MaterialStateProperty.resolveWith((Set<MaterialState> states) {
+                return states.contains(MaterialState.disabled) ? null : Colors.green;
+              }),
+            ),
+            onPressed: _showWelcomeScreen,
+            child: const Text('Inscreva-se'),
+          ),
+        ],
+      ),
+    );
+  }
+}
